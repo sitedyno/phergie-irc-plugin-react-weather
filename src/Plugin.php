9fbd4e0b6b6f6545159c232550c4e9d948f20caa
Heath Nail <sitedyno@gmail.com>
2016-12-21 22:56:24 -0500

    Rejection callback receives ClientException object

--- src/Plugin.php
+++ src/Plugin.php
@@ -14,6 +14,7 @@ use Phergie\Irc\Bot\React\AbstractPlugin;
 use Phergie\Irc\Bot\React\EventQueueInterface as Queue;
 use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
 use GuzzleHttp\Message\Response;
+use GuzzleHttp\Exception\ClientException;
 use Phergie\Plugin\Http\Request as HttpRequest;
 use Chrismou\Phergie\Plugin\Weather\Provider\WeatherProviderInterface;
 
@@ -111,7 +112,7 @@ class Plugin extends AbstractPlugin
             'resolveCallback' => function (Response $response) use ($self, $event, $queue) {
                 $self->sendIrcResponse($event, $queue, $this->provider->getSuccessLines($event, $response->getBody()));
             },
-            'rejectCallback' => function (Response $error) use ($self, $event, $queue) {
+            'rejectCallback' => function (ClientException $error) use ($self, $event, $queue) {
                 $self->sendIrcResponse($event, $queue, $this->provider->getRejectLines($event, $error->getBody()));
             }
         ));
